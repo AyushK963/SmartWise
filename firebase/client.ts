da327d08fd7f86f06e5e6fb5a1c11eabd511468d
Ayush-97k <ayush.kankane2020@vitstudent.ac.in>
2025-04-15 23:59:18 +0530

    last commit

--- firebase/client.ts
+++ firebase/client.ts
@@ -9,13 +9,20 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: "prepwise-f28d1.firebaseapp.com",
-  projectId: "prepwise-f28d1",
-  storageBucket: "prepwise-f28d1.firebasestorage.app",
-  messagingSenderId: "294006253048",
-  appId: "1:294006253048:web:8ee49e64be3ef0f103e20d",
-  measurementId: "G-GEKHT6HWKQ",
+  // apiKey: process.env.FIREBASE_API_KEY,
+  // authDomain: "prepwise-f28d1.firebaseapp.com",
+  // projectId: "prepwise-f28d1",
+  // storageBucket: "prepwise-f28d1.firebasestorage.app",
+  // messagingSenderId: "294006253048",
+  // appId: "1:294006253048:web:8ee49e64be3ef0f103e20d",
+  // measurementId: "G-GEKHT6HWKQ",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
